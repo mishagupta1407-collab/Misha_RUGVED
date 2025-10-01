98fccbb0418fba58cdcb984e1a7cb6990ba6fe45
Misha <mishagupta.1407@gmail.com>
2025-10-01 14:17:30 +0530

    Delete task1.py

--- task1.py
+++
@@ -1,8 +0,0 @@
-#Define a function named “triple_and” that takes three parameters and returns True only if they are all True and False otherwise
-
-def triple_and(a,b,c):
-    return bool(a) and bool(b) and bool(c)
-a=input("Enter first value: ")
-b=input("Enter second value: ")
-c=input("Enter third value: ")
-print(triple_and(a,b,c))
